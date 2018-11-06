42e4f060c917638eb9c02f682171ed674040328e
Chris Underwood <cunderwoodmn@gmail.com>
2018-11-06 11:32:35 -0600

    add ls and pwd

--- bash.js
+++ bash.js
@@ -1,10 +1,19 @@
+const pwd = require('./pwd');
+const ls = require('./ls');
+
 // output a prompt
+
 process.stdout.write('prompt >');
 
-process.stdin.on( 'data', (data)=>{
-    const cmd = data.toString().trim(); // remote the newline
+process.stdin.on('data', data => {
+  const cmd = data.toString().trim(); // remote the newline
 
-    process.stdout.write('You typed: ' + cmd); 
-    process.stdout.write('\nprompt > ');
+  if (cmd === 'pwd') {
+    pwd();
+  } else if (cmd === 'ls') {
+    ls();
+  }
 
-});    
+  process.stdout.write('You typed: ' + cmd);
+  process.stdout.write('\nprompt > ');
+});
