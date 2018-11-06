721f211387c02de955f748e23019d726d81b277f
Chris Underwood <cunderwoodmn@gmail.com>
2018-11-06 11:48:39 -0600

    add cat.js

--- bash.js
+++ bash.js
@@ -1,17 +1,26 @@
 const pwd = require('./pwd');
 const ls = require('./ls');
+const cat = require('./cat');
 
 // output a prompt
 
 process.stdout.write('prompt >');
 
 process.stdin.on('data', data => {
-  const cmd = data.toString().trim(); // remote the newline
+  const cmdArr = data
+    .toString()
+    .trim()
+    .split(' ');
+  console.log(cmdArr);
+  const cmd = cmdArr[0].toString().trim(); // remote the newline
+  const fileName = cmdArr[1].toString().trim();
 
   if (cmd === 'pwd') {
     pwd();
   } else if (cmd === 'ls') {
     ls();
+  } else if (cmd === 'cat') {
+    cat(fileName);
   }
 
   process.stdout.write('You typed: ' + cmd);
